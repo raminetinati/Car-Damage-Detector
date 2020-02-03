45ef149fd3d35fda3e2778ca0d08416da85d6404
EC2 Default User <ec2-user@ip-172-16-92-89.ec2.internal>
2020-02-03 03:02:08 +0000

    Added CarObjectDetector Code

--- objectdetector.py
+++ objectdetector.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import sagemaker
 from sagemaker.tensorflow import TensorFlowPredictor
-
+import requests
+import shutil
+import json
+import numpy as np
 
 class ObjectDetector:
 
@@ -33,25 +36,20 @@ class ObjectDetector:
 		print('Added {} Class Labels'.format(len(self.object_categories)))
 
 
-	def display_test_img(self, bucket_name, test_manifest):
-		
-		root = 'training/'
-		test_count = len(test_manifest)
-		random_annotation = test_manifest[random.randint(0,test_count-1)]
-		s3_uri = random_annotation['path']
-		
-		s3_key = os.path.basename(s3_uri)
-		local_path = 'images/' + s3_key
-	#     print(s3_key)
-		s3.Bucket(bucket_name).download_file(
-		root+s3_key, local_path)
-		
-		return s3_key, local_path
+	def test_model_on_manifest_data(self, bucket_name, manifest):
+
+		image_path = self.generate_random_image_file_from_manifest(bucket_name, manifest)
+		self.visualize_detection(image_path)
+
+	def test_model_on_image(self, url_of_image):
+
+		image_path = download_image(self, url, to_save_filename='test-image.png')
+		self.visualize_detection(image_path)
 
 
-	def visualize_detection(self, bucket_name, test_manifest, thresh=0.6):
+	def visualize_detection(self, image_path, thresh=0.6):
 
-		s3_key, local_path = self.display_test_img(bucket_name, test_manifest)
+		local_path = image_path
 
 		with open(local_path, 'rb') as f:
 			img_file = f.read()
@@ -99,6 +97,35 @@ class ObjectDetector:
 
 		plt.show()
 		return det_classes_and_score
+
+
+	def generate_random_image_file_from_manifest(self, bucket_name, test_manifest, manifest = 'validation'):
+		
+		root = manifest
+		test_count = len(test_manifest)
+		random_annotation = test_manifest[random.randint(0,test_count-1)]
+		s3_uri = random_annotation['path']
+		
+		s3_key = os.path.basename(s3_uri)
+		local_path = 'images/' + s3_key
+	#     print(s3_key)
+		s3.Bucket(bucket_name).download_file(
+		root+s3_key, local_path)
+		
+		return local_path
+
+
+	def download_image(self, url, to_save_filename='test-image.png'):
+
+		print('downloading Image from {}'.format(url))
+		response = requests.get(url, stream=True)
+		with open(to_save_filename, 'wb') as out_file:
+			shutil.copyfileobj(response.raw, out_file)
+		print('Saved Image to {}'.format(to_save_filename)) 
+		del response
+		return to_save_filename
+
+
 
 
 
